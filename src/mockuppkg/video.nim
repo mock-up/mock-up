4f45f0e9ab956a190433a3aac158d6971afed71f
momeemt <momiimt@gmail.com>
2021-05-03 22:53:07 +0900

    👍  add: impl video decoder

--- src/mockuppkg/video.nim
+++ src/mockuppkg/video.nim
@@ -1,8 +1,16 @@
-import ffmpeg, options
+import ffmpeg, options, deques
 
 type
   mVideo* = object
+    path: string
+    format_context: ptr AVFormatContext
+    codec_param: ptr AVCodecParameters
     codec_context: ptr AVCodecContext
+    video_codec: ptr AVCodec
+    video_context: ptr AVCodecContext
+    stream: ptr AVStream
+    frames: Deque[ptr AVFrame]
+    time_base: AVRational
 
 proc width* (video: mVideo): uint =
   result = video.codec_context.width.uint
@@ -10,58 +18,87 @@ proc width* (video: mVideo): uint =
 proc height* (video: mVideo): uint =
   result = video.codec_context.height.uint
 
-proc on_frame_decoded (frame: ptr AVFrame) =
-  echo frame.pts
-
-proc Video* (path: string): Option[mVideo] =
-  var format_context = avformat_alloc_context()
-  
-  # 動画ファイルを開いて format_context に格納
-  if avformat_open_input(addr format_context, path, nil, nil) != 0:
-    echo "[Runtime]: avformat_open_input failed"
-    return none(mVideo)
-  
-  # 動画ファイルのストリーム情報取得
-  if avformat_find_stream_info(format_context, nil) < 0:
-    echo "[Runtime]: avformat_find_stream_info failed"
+template checkCorrectMVideo (condition: bool, msg: string): untyped =
+  if condition:
+    stderr.writeLine("[Runtime]: " & msg)
     return none(mVideo)
 
+proc getFormatContext (video: var mVideo): bool =
+  result = avformat_open_input(addr video.format_context, video.path, nil, nil) != 0
+
+proc getStreamInfo (video: var mVideo): bool =
+  result = avformat_find_stream_info(video.format_context, nil) < 0
+
+proc getDecoder (video: var mVideo): bool =
   var
-    streams = cast[ptr UncheckedArray[ptr AVStream]](format_context[].streams)
+    streams = cast[ptr UncheckedArray[ptr AVStream]](video.format_context[].streams)
     stream_id = -1
     codec_param: ptr AVCodecParameters = nil
     video_codec: ptr AVCodec = nil
   
-  for frame in 0 ..< format_context[].nb_streams:
+  for stream in 0 ..< video.format_context[].nb_streams:
     var
-      locpar = streams[frame][].codecpar
+      locpar = streams[stream][].codecpar
       locdec = avcodec_find_decoder(locpar[].codec_id)
     if locpar[].codec_type == AVMEDIA_TYPE_VIDEO:
       video_codec = locdec
       codec_param = locpar
-      stream_id = frame.int
+      stream_id = stream.int
       break
   
-  # デコード
-  var
-    video_context = avcodec_alloc_context3(video_codec)
-    frame = av_frame_alloc()
-    packet = AVPacket()
-  
-  if avcodec_parameters_to_context(video_context, codec_param) < 0:
-    echo "[Runtime]: avcodec_parameters_to_context failed"
-    return none(mVideo)
+  video.codec_param = codec_param
+  video.video_codec = video_codec
+  video.stream = streams[stream_id]
 
-  if avcodec_open2(video_context, video_codec, nil) < 0:
-    echo "[Runtime]: avcodec_open2 failed"
-    return none(mVideo)
+  result = not(stream_id == -1 or codec_param == nil or video_codec == nil)
+
+proc initializeAVCodecContext (video: var mVideo): bool =
+  result = avcodec_open2(video.video_context, video.video_codec, nil) < 0
+
+proc openAVCodecParameters (video: var mVideo): bool =
+  result = avcodec_parameters_to_context(video.video_context, video.codec_param) < 0
+
+proc encode* (output_path: string, video: mVideo): bool =
+  result = true
+
+proc decode* (video: var mVideo): Option[mVideo] =
+  var
+    frames = initDeque[ptr AVFrame]()
+    packet = AVPacket()
+    frame = av_frame_alloc()
   
-  while av_read_frame(format_context, addr packet) == 0:
-    if packet.stream_index == streams[stream_id].index:
-      if avcodec_send_packet(video_context, addr packet) != 0:
+  checkCorrectMVideo(video.getFormatContext, "動画ファイルを開けませんでした")
+  checkCorrectMVideo(video.getStreamInfo, "動画ファイルのストリーム情報取得に失敗しました")
+  checkCorrectMVideo(video.getDecoder, "デコーダの取得に失敗しました")
+
+  # デコード
+  video.time_base = video.stream.time_base
+  video.video_context = avcodec_alloc_context3(video.video_codec)
+  checkCorrectMVideo(video.openAVCodecParameters, "コーデックパラメータを開けませんでした")
+  checkCorrectMVideo(video.initializeAVCodecContext, "コーデックの初期化に失敗しました")
+
+  while av_read_frame(video.format_context, addr packet) == 0:
+    if packet.stream_index == video.stream.index:
+      if avcodec_send_packet(video.video_context, addr packet) != 0:
         return none(mVideo)
-      while avcodec_receive_frame(video_context, frame) == 0:
-        on_frame_decoded(frame)
+      while avcodec_receive_frame(video.video_context, frame) == 0:
+        var new_ref = av_frame_alloc()
+        discard av_frame_ref(new_ref, frame)
+        frames.addLast(new_ref)
     av_packet_unref(addr packet)
-  
-  result = some(mVideo(codec_context: video_context))
+
+  video.frames = frames
+  result = some(video)
+
+proc Video* (path: string): mVideo =
+  result = mVideo(
+    path: path,
+    format_context: avformat_alloc_context(),
+    codec_param: nil,
+    codec_context: nil,
+    video_codec: nil,
+    video_context: nil,
+    stream: nil,
+    frames: initDeque[ptr AVFrame](),
+    time_base: AVRational()
+  )
