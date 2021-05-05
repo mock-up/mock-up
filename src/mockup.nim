88269f587ed0d51dac78cd1661fbac0f987bfc37
momeemt <momiimt@gmail.com>
2021-05-05 14:49:01 +0900

    👍  Add: impl generating backend

--- src/mockup.nim
+++ src/mockup.nim
@@ -5,9 +5,10 @@ when isMainModule:
     createDir("frontend")
     createDir("backend")
     let frontend_dir = currentSourcePath.parentDir().parentDir() & "/project/frontend"
-    echo frontend_dir
     copyDir(frontend_dir, "frontend")
     discard execShellCmd("yarn --cwd frontend install")
+    let backend_dir = currentSourcePath.parentDir().parentDir() & "/project/backend"
+    copyDir(backend_dir, "backend")
 
   proc dev (): int =
     discard execShellCmd("yarn --cwd frontend start")
