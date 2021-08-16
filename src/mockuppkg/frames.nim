22da9fd574f1fd764ec5b060ff905323da01af8a
Mutsuha Asada <momiimt@gmail.com>
2021-08-16 20:14:39 +0900

    👍 Add: implement FrameEither

--- src/mockuppkg/frames.nim
+++ src/mockuppkg/frames.nim
@@ -1,14 +1,25 @@
 from ffmpeg import nil
+from fp import nil
+from Palette import nil
 
 type
   Frame* = object
     av_frame: ptr ffmpeg.AVFrame
+  FrameEither* = fp.Either[string, Frame]
 
-proc initFrame (height, width: int32): Frame =
-  result.av_frame = ffmpeg.av_frame_alloc()
-  result.av_frame[].format = ffmpeg.AV_PIX_FMT_RGB24.cint
-  result.av_frame[].height = height
-  result.av_frame[].width = width
+proc getFrameEither (frame_either: FrameEither): Frame =
+  result = fp.get[string, Frame](frame_either)
+
+proc getAVFrameInFrameEither (frame_either: FrameEither): ptr ffmpeg.AVFrame =
+  result = frame_either.getFrameEither.av_frame
+
+proc initFrame* (height, width: int32): FrameEither =
+  result = fp.Right[string, Frame](Frame(av_frame: ffmpeg.av_frame_alloc()))
+  result.getAVFrameInFrameEither[].format = ffmpeg.AV_PIX_FMT_RGB24.cint
+  result.getAVFrameInFrameEither[].height = height
+  result.getAVFrameInFrameEither[].width = width
+  if ffmpeg.av_frame_get_buffer(fp.get[string, Frame](result).av_frame, 32) < 0:
+    result = fp.Left[string, Frame]("Error")
 
 proc copy* (src: Frame): Frame =
   result.av_frame = ffmpeg.av_frame_alloc()
