edca010961587781ca95bb22d02609f398a5d107
Mutsuha Asada <momiimt@gmail.com>
2021-08-16 20:22:29 +0900

    👍 Add: implement Pixel

--- src/mockuppkg/frames.nim
+++ src/mockuppkg/frames.nim
@@ -6,6 +6,10 @@ type
   Frame* = object
     av_frame: ptr ffmpeg.AVFrame
   FrameEither* = fp.Either[string, Frame]
+  PixelRowData = tuple[x: int, linesize: cint, datum: ptr uint8]
+  Pixel* = object
+    x, y: int
+    red, green, blue: ptr uint8
 
 proc getFrameEither (frame_either: FrameEither): Frame =
   result = fp.get[string, Frame](frame_either)
@@ -30,3 +34,27 @@ proc copy* (src: Frame): Frame =
   result.av_frame[].channel_layout = src.av_frame[].channel_layout
   result.av_frame[].nb_samples = src.av_frame[].nb_samples
   result.av_frame[].pts = src.av_frame[].pts
+
+proc `[]`* (frame: Frame, x: int): PixelRowData =
+  result = (x: x, linesize: frame.av_frame[].linesize[0], datum: frame.av_frame[].data[0])
+
+proc `[]`* (pixelRowData: PixelRowData, y: int): Pixel =
+  var index = (y * pixelRowData.linesize + pixelRowData.x * 3).uint
+  result = Pixel(
+    x: pixelRowData.x, y: y,
+    red: cast[ptr uint8](cast[uint](pixelRowData.datum) + index),
+    green: cast[ptr uint8](cast[uint](pixelRowData.datum) + index + 1),
+    blue: cast[ptr uint8](cast[uint](pixelRowData.datum) + index + 2)
+  )
+
+proc rgb* (pixel: Pixel): Palette.tRGB =
+  result = (
+    Palette.tBinaryRange(pixel.red[]),
+    Palette.tBinaryRange(pixel.green[]),
+    Palette.tBinaryRange(pixel.blue[])
+  )
+
+proc `rgb=`* (pixel: Pixel, color: Palette.tRGB): Palette.tRGB =
+  pixel.red[] = color.red.uint8
+  pixel.green[] = color.green.uint8
+  pixel.blue[] = color.blue.uint8
