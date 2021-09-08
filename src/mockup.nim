542b1f3f06cb9dada2c2eca70a3f5a6594f9187d
Mutsuha Asada <momiimt@gmail.com>
2021-09-09 07:08:52 +0900

    👍 Add: mid-presentation demostration
    中間発表デモのためにmumlを受け取るサーバーを実装

--- src/mockup.nim
+++ src/mockup.nim
@@ -1,7 +1,11 @@
 when isMainModule:
-  import cligen, os
+  import cligen, os, json
+  import jester
+  import mockuppkg/[video, timeline, fonts]
+  import muml
+  from fp import either
 
-  proc init (): int =
+  proc init2 (): int =
     createDir("frontend")
     createDir("backend")
     discard execShellCmd("cd frontend && git init")
@@ -11,9 +15,25 @@ when isMainModule:
     discard execShellCmd("cd backend && git pull https://github.com/mock-up/mock-up-backend-template.git")
 
   proc dev (): int =
-    discard execShellCmd("cd frontend && npm run start")
+    var video = Video("assets/sample2.mp4")
+    let muml = muml("assets/muml.json")
+    echo video.output("assets/new_encoder.mp4", muml)
+
+  proc server (): int =
+    routes:
+      post "/muml":
+        var timeline = mTimeLine()
+        let muml = request.body.parseJson.muml
+        # for muml_obj in muml.content.element:
+        #   timeline.add muml_obj
+        # timeline.header = muml.header.getHeader
+        # echo timeline.encode("assets/new_encoder.mp4")
+        var video = Video("assets/src.mp4")
+        echo video.output("assets/out/mitou1.mp4", muml)
+        resp %*{"message": "Created"}
 
   dispatchMulti(
-    [init, cmdName = "new"],
-    [dev]
+    [init2, cmdName = "new"],
+    [dev],
+    [server]
   )
