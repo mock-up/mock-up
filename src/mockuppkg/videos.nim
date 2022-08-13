15654bf36c95a604648c5e35e4baafabde5c49ef
Mutsuha Asada <momiimt@gmail.com>
2022-08-13 23:08:23 +0900

    👍mockupVideo型からエンコード関連の要素/proceduresを排除
    動画が引き伸ばされて描画されていたのでプロジェクトサイズに対する描画ができるように実装した

--- src/mockuppkg/videos.nim
+++ src/mockuppkg/videos.nim
@@ -1,139 +1,30 @@
-from ffmpeg import nil
-import images
+import frames
+import ffmpeg
+import nagu
+import muml
+import glm
+import utils
 
 type
-  MockupVideo* = object
-    path: string
-    io_context: ptr ffmpeg.AVIOContext
-    format_context: ptr ffmpeg.AVFormatContext
-    codec_param: ptr ffmpeg.AVCodecParameters
-    codec: ptr ffmpeg.AVCodec
-    stream: ptr ffmpeg.AVStream
-    time_base: ffmpeg.AVRational
-    codec_context*: ptr ffmpeg.AVCodecContext
-    programID: uint32
-    encoder_codec: ptr ffmpeg.AVCodec
-    encoder_codec_context: ptr ffmpeg.AVCodecContext
-    encoder_stream: ptr ffmpeg.AVStream
-    encoder_format_context: ptr ffmpeg.AVFormatContext
+  mockupVideo* = object
+    format_context: ptr AVFormatContext
+    codec: ptr AVCodec
+    codec_param: ptr AVCodecParameters
+    codec_context: ptr AVCodecContext
+    stream: ptr AVStream
+    vertex_shader_path: string
+    fragment_shader_path: string
+    positions: array[4, Vec3[int]]
+    frame_counter: tuple[start, stop: int]
+    decoded_frames: seq[ptr AVFrame]
 
-proc width* (video: MockupVideo): int32 =
+proc width* (video: mockupVideo): int32 =
   result = video.codec_context.width.int32
 
-proc height* (video: MockupVideo): int32 =
+proc height* (video: mockupVideo): int32 =
   result = video.codec_context.height.int32
 
-proc setFormatContext (video: var MockupVideo) =
-  if ffmpeg.avformat_open_input(video.format_context.addr, video.path, nil, nil) != 0:
-    raise newException(FFmpegError, "動画ファイルを開けませんでした")
-
-proc setStreamInfo (video: var MockupVideo) =
-  if ffmpeg.avformat_find_stream_info(video.format_context, nil) < 0:
-    raise newException(FFmpegError, "ストリーム情報取得に失敗しました")
-
-proc setDecoder (video: var MockupVideo) =
-  let streams = cast[ptr UncheckedArray[ptr ffmpeg.AVStream]](video.format_context[].streams)
-  for stream_index in 0 ..< video.format_context[].nb_streams:
-    let locpar = streams[stream_index][].codecpar
-    if locpar[].codec_type == ffmpeg.AVMEDIA_TYPE_VIDEO:
-      video.codec = ffmpeg.avcodec_find_decoder(locpar[].codec_id)
-      video.codec_param = locpar
-      video.stream = streams[stream_index]
-      break
-  if video.codec == nil or video.codec_param == nil:
-    raise newException(FFmpegError, "デコーダの取得に失敗しました")
-
-proc initializeCodecContextParams (video: var MockupVideo) =
-  if ffmpeg.avcodec_parameters_to_context(video.codec_context, video.codec_param) < 0:
-    raise newException(FFmpegError, "コーデックパラメータの初期化に失敗しました")
-
-proc initializeCodecContext (video: var MockupVideo) =
-  if ffmpeg.avcodec_open2(video.codec_context, video.codec, nil) < 0:
-    raise newException(FFmpegError, "コーデックの初期化に失敗しました")
-
-proc prepareDecode (video: var MockupVideo) =
-  video.setFormatContext
-  video.setStreamInfo
-  video.setDecoder
-  video.time_base = video.stream[].time_base
-  video.codec_context = ffmpeg.avcodec_alloc_context3(video.codec)
-  video.initializeCodecContextParams
-  video.initializeCodecContext
-
-proc setEncoder (video: var MockupVideo) =
-  var codec = ffmpeg.avcodec_find_encoder(ffmpeg.AV_CODEC_ID_H264)
-  if codec == nil:
-    raise newException(FFmpegError, "エンコーダが見つかりませんでした")
-  video.encoder_codec = codec
-
-proc setEncoderCodecContext (video: var MockupVideo) =
-  var codec_context = ffmpeg.avcodec_alloc_context3(video.encoder_codec)
-  if codec_context == nil:
-    raise newException(FFmpegError, "コーデックの割り当てに失敗しました")
-  video.encoder_codec_context = codec_context
-
-proc prepareMp4Encode (video: var MockupVideo, frame: ptr ffmpeg.AVFrame) =
-  video.setEncoder
-  video.setEncoderCodecContext
-  video.encoder_codec_context.pix_fmt = ffmpeg.AVPixelFormat(frame.format.int)
-  video.encoder_codec_context.width = frame.width
-  video.encoder_codec_context.height = frame.height
-  video.encoder_codec_context.field_order = ffmpeg.AV_FIELD_PROGRESSIVE
-  video.encoder_codec_context.color_range = frame.color_range
-  video.encoder_codec_context.color_primaries = frame.color_primaries
-  video.encoder_codec_context.color_trc = frame.color_trc
-  video.encoder_codec_context.colorspace = frame.colorspace
-  video.encoder_codec_context.chroma_sample_location = frame.chroma_location
-  video.encoder_codec_context.sample_aspect_ratio = frame.sample_aspect_ratio
-  video.encoder_codec_context.time_base = video.time_base
-  var codec_options: ptr ffmpeg.AVDictionary = nil
-  discard ffmpeg.av_dict_set(codec_options.addr, "profile", "high", 0)
-  discard ffmpeg.av_dict_set(codec_options.addr, "preset", "medium", 0)
-  discard ffmpeg.av_dict_set(codec_options.addr, "crf", "22", 0)
-  discard ffmpeg.av_dict_set(codec_options.addr, "level", "4.0", 0)
-
-  if ffmpeg.avcodec_open2(video.encoder_codec_context, video.encoder_codec_context[].codec, codec_options.addr) < 0:
-    raise newException(FFmpegError, "コーデックコンテキストの初期化に失敗しました")
-
-proc newVideo* (path: string, programID: uint32): MockupVideo =
-  result.path = path
-  result.programID = programID
-  result.prepareDecode
-  if ffmpeg.avio_open(
-    result.io_context.addr, "./assets/out/encode.mp4", ffmpeg.AVIO_FLAG_WRITE
-  ) < 0:
-    raise newException(FFmpegError, "IOコンテキストの初期化に失敗しました")
-  if ffmpeg.avformat_alloc_output_context2(
-    result.encoder_format_context.addr, nil, "mp4", nil
-  ) < 0:
-    raise newException(FFmpegError, "出力フォーマットへのコンテキスト割り当てに失敗しました")
-  result.encoder_format_context.pb = result.io_context
-
-proc getFormatConverter (video: MockupVideo, pixel_format: ffmpeg.AVPixelFormat): ptr ffmpeg.SwsContext =
-  result = ffmpeg.sws_getContext(
-    video.codec_context[].width,
-    video.codec_context[].height,
-    video.codec_context[].pix_fmt,
-    video.codec_context[].width,
-    video.codec_context[].height,
-    pixel_format,
-    ffmpeg.SWS_BICUBIC,
-    nil, nil, nil
-  )
-
-proc getEncoderSwscontext* (codecContext: ptr ffmpeg.AVCodecContext, pixelFormat: ffmpeg.AVPixelFormat): ptr ffmpeg.SwsContext =
-  result = ffmpeg.sws_getContext(
-    codecContext[].width,
-    codecContext[].height,
-    pixelFormat,
-    codecContext[].width,
-    codecContext[].height,
-    codecContext[].pix_fmt,
-    ffmpeg.SWS_BICUBIC,
-    nil, nil, nil
-  )
-
-proc formatConvert (src: ptr ffmpeg.AVFrame, format_converter: ptr ffmpeg.SwsContext): ptr ffmpeg.AVFrame =
+proc formatConvert (src: ptr AVFrame, format_converter: ptr SwsContext): ptr AVFrame =
   result = prepareCopyFrame(src)
   result[].format = ffmpeg.AV_PIX_FMT_RGBA.cint
   if ffmpeg.av_frame_get_buffer(result, 32) < 0:
@@ -148,55 +39,149 @@ proc formatConvert (src: ptr ffmpeg.AVFrame, format_converter: ptr ffmpeg.SwsCon
     result[].linesize[0].addr
   )
 
-proc seek* (video: var MockupVideo, timestamp: int) =
-  if ffmpeg.av_seek_frame(video.format_context, video.stream[].index, timestamp, ffmpeg.AVSEEK_FLAG_BACKWARD) < 0:
-    echo "av_seek_frame failed"
-  ffmpeg.avcodec_flush_buffers(video.codec_context)
+proc init* (T: type mockupVideo,
+            position: Vec3[int],
+            src_path: string,
+            vertex_shader_path: string,
+            fragment_shader_path: string): mockupVideo =
+  result.vertex_shader_path = vertex_shader_path
+  result.fragment_shader_path = fragment_shader_path
+  result.frame_counter = (-1, -1)
+  if avformat_open_input(result.format_context.addr, src_path, nil, nil) != 0:
+    raise newException(Defect, "動画ファイルを開けませんでした")
+  
+  if avformat_find_stream_info(result.format_context, nil) < 0:
+    raise newException(Defect, "ストリーム情報取得に失敗しました")
 
-iterator items* (video: var MockupVideo): MockupImage =
-  ## 与えられた動画のフレームを全て返却する
-  var packet = ffmpeg.AVPacket()
-  var once = true
-  var image: MockupImage
-  var format_converter = video.getFormatConverter(ffmpeg.AV_PIX_FMT_RGBA)
-  while ffmpeg.av_read_frame(video.format_context, packet.addr) == 0:
+  let streams = cast[ptr UncheckedArray[ptr AVStream]](result.format_context[].streams)
+  for stream_index in 0 ..< result.format_context[].nb_streams:
+    let locpar = streams[stream_index][].codecpar
+    if locpar[].codec_type == AVMEDIA_TYPE_VIDEO:
+      result.codec = avcodec_find_decoder(locpar[].codec_id)
+      result.codec_param = locpar
+      result.stream = streams[stream_index]
+      break
+  if result.codec.isNil or result.codec_param.isNil:
+    raise newException(Defect, "デコーダの取得に失敗しました")
+
+  result.codec_context = avcodec_alloc_context3(result.codec)
+  
+  if avcodec_parameters_to_context(result.codec_context, result.codec_param) < 0:
+    raise newException(Defect, "コーデックパラメータの初期化に失敗しました")
+
+  if avcodec_open2(result.codec_context, result.codec, nil) < 0:
+    raise newException(Defect, "コーデックの初期化に失敗しました")
+  
+  let (x, y, z) = (
+    (result.width) div 2 + position[0],
+    (result.height) div 2 + position[1],
+    position[2]
+  )
+  result.positions = [
+    vec3(-x, y, z),
+    vec3(-x, -y, z),
+    vec3(x, -y, z),
+    vec3(x, y, z),
+  ]
+
+func frameNumber (video: mockupVideo): int =
+  # `video`が持つフレーム数を返します
+  result = video.codec_context[].frame_number
+
+proc decode* (video: var mockupVideo, header: mumlHeader, frameCount: int): mockupFrame =
+  var
+    packet = AVPacket()
+    format_converter = sws_getContext(
+      video.codec_context[].width,
+      video.codec_context[].height,
+      video.codec_context[].pix_fmt,
+      video.codec_context[].width,
+      video.codec_context[].height,
+      AV_PIX_FMT_RGBA,
+      SWS_BICUBIC,
+      nil, nil, nil
+    )
+  var image = mockupFrame.init(
+    header,
+    video.positions,
+    video.vertex_shader_path,
+    video.fragment_shader_path
+  )
+
+
+  if video.frame_counter.start <= frameCount and frameCount <= video.frame_counter.stop:
+    var frame = video.decoded_frames[frameCount - video.frame_counter.start]
+    image.naguTexture.use do (texture: var naguBindedTexture):
+      texture.pixels = (data: frame.data[0], width: frame[].width.uint, height: frame[].height.uint)
+    return image
+
+  var frame_counter = video.frame_counter.stop
+  video.frame_counter.start = video.frame_counter.stop + 1
+  video.decoded_frames = @[]
+  
+  while av_read_frame(video.format_context, packet.addr) == 0:
     if packet.stream_index != video.stream.index:
-      ffmpeg.av_packet_unref(packet.addr)
+      av_packet_unref(packet.addr)
       continue
-    if ffmpeg.avcodec_send_packet(video.codec_context, packet.addr) != 0:
-      raise newException(FFmpegError, "パケットの取り出しに失敗しました")
-    var frame = ffmpeg.av_frame_alloc()
-    while ffmpeg.avcodec_receive_frame(video.codec_context, frame) == 0:
+    if avcodec_send_packet(video.codec_context, packet.addr) != 0:
+      raise newException(Defect, "パケットの取り出しに失敗しました")
+    var frame = av_frame_alloc()
+    while avcodec_receive_frame(video.codec_context, frame) == 0:
       var
         copy_frame = frame.copy # 元ポインタを直接操作するとバグる
         frame_RGBA = copy_frame.formatConvert(format_converter) # RGBAに変換する
+      frame_counter += 1
+      video.decoded_frames.add frame_RGBA
+    av_frame_free(frame.addr)
+  av_packet_unref(packet.addr)
+  video.frame_counter.stop = frame_counter
 
-      if once:
-        image = newImage(frame_RGBA, video.programID)
+  echo frame_counter
+  var frame = video.decoded_frames[0]
+  image.naguTexture.use do (texture: var naguBindedTexture):
+    texture.pixels = (data: frame.data[0], width: frame[].width.uint, height: frame[].height.uint)
+  return image
 
-        var ref_frame = ffmpeg.av_frame_alloc()
-        discard ffmpeg.av_frame_ref(ref_frame, frame)
-        
-        video.prepareMp4Encode(ref_frame)
-        video.encoder_stream = ffmpeg.avformat_new_stream(
-          video.encoder_format_context, video.encoder_codec
-        )
-        if video.encoder_stream == nil:
-          raise newException(FFmpegError, "ストリームの取得に失敗しました")
-        video.encoder_stream.sample_aspect_ratio = video.encoder_codec_context.sample_aspect_ratio
-        video.encoder_stream.time_base = video.encoder_codec_context.time_base
-
-        if ffmpeg.avcodec_parameters_from_context(
-          video.encoder_stream.codecpar, video.encoder_codec_context
-        ) < 0:
-          raise newException(FFmpegError, "コーデックコンテキストによる塗りつぶしに失敗しました")
-        
-        if ffmpeg.avformat_write_header(video.encoder_format_context, nil) < 0:
-          raise newException(FFmpegError, "ヘッダーの書き込みに失敗しました")
-
-        once = false
-
-      image.updateImage(frame_RGBA)
+iterator decodeVideo* (video: var mockupVideo, header: mumlHeader): mockupFrame =
+  var
+    packet = AVPacket()
+    format_converter = sws_getContext(
+      video.codec_context[].width,
+      video.codec_context[].height,
+      video.codec_context[].pix_fmt,
+      video.codec_context[].width,
+      video.codec_context[].height,
+      AV_PIX_FMT_RGBA,
+      SWS_BICUBIC,
+      nil, nil, nil
+    )
+  var image = mockupFrame.init(
+    header,
+    video.positions,
+    video.vertex_shader_path,
+    video.fragment_shader_path
+  )
+  while av_read_frame(video.format_context, packet.addr) == 0:
+    if packet.stream_index != video.stream.index:
+      av_packet_unref(packet.addr)
+      continue
+    if avcodec_send_packet(video.codec_context, packet.addr) != 0:
+      raise newException(Defect, "パケットの取り出しに失敗しました")
+    var frame = av_frame_alloc()
+    while avcodec_receive_frame(video.codec_context, frame) == 0:
+      var
+        copy_frame = frame.copy # 元ポインタを直接操作するとバグる
+        frame_RGBA = copy_frame.formatConvert(format_converter) # RGBAに変換する
+      image.naguTexture.use do (texture: var naguBindedTexture):
+        texture.pixels = (data: frame_RGBA.data[0], width: frame_RGBA[].width.uint, height: frame_RGBA[].height.uint)
+      
+      if frame_RGBA[].key_frame == 1:
+        echo "keyframe: ", frame_RGBA.pts
       yield image
-    ffmpeg.av_frame_free(frame.addr)
-  ffmpeg.av_packet_unref(packet.addr)
+    av_frame_free(frame.addr)
+  av_packet_unref(packet.addr)
+
+proc seek* (video: var mockupVideo, frame: int) =
+  if av_seek_frame(video.format_context, video.stream[].index, frame, AVSEEK_FLAG_BACKWARD) < 0:
+    echo "av_seek_frame failed"
+  avcodec_flush_buffers(video.codec_context)
